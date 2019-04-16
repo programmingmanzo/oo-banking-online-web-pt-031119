53f0e335935e8c5f644b3b8a69218132f2fe7aad
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-16 07:42:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,14 +3,17 @@ require 'pry'
 class Transfer
   # your code here
   
-  attr_accessor :sender, :receiver, :transfer 
+  attr_accessor :sender, :receiver, :transfer, :status 
   
   def initialize(sender, receiver, transfer)
     @sender =  sender 
     @receiver = receiver
     @transfer = self 
+    @status = "pending"
      #binding.pry 
   end 
+  
+  
   
   
 end
