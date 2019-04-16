007afffe4976176b82a9ae1ba78a77ec26bb1208
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-16 07:35:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -4,7 +4,7 @@ class Transfer
   attr_accessor :transfer, :sender, :receiver 
   
   def initialize(transfer, sender, receiver)
-    @sender = self.sender 
+    @sender = transfer.sender 
     @receiver = receiver
     @transfer = self 
      
